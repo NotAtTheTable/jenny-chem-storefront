1eabf562104f0755c8bde703e4683aa5efca3ec2
Rob <robert@abbeybarn.com>
2024-06-18 14:21:06 +0100

    Fixed some more bubble rendering

--- app/components/foundational/BlueBubbleBackground.tsx
+++ app/components/foundational/BlueBubbleBackground.tsx
@@ -4,7 +4,7 @@ import "./BlueBubbleBackground.css"
 export function BlueBubbleBackground({ children }: { children: React.ReactNode }) {
     return <div className='relative bg-jc-dark-blue-100 overflow-hidden z-0'>
         <img src={LightBlueBubble} alt="left Bubble Background" className='absolute z-1 h-[300%] top-[-100%] -translate-x-1/2 hidden sm:block' />
-        <img src={LightBlueBubble} alt="right Bubble Background" className='absolute z-1 min-w-[200%] -left-2/4 bottom-0 translate-y-2/3 rotate-90 sm:h-[300%] sm:top-[-100%] sm:left-[100%] sm:-translate-x-1/2' />
+        <img src={LightBlueBubble} alt="right Bubble Background" className='absolute z-1 min-w-[200%] -left-2/4 bottom-0 translate-y-2/3  rotate-90 sm:min-w-[100%] sm:translate-y-0 sm:rotate-0 sm:h-[300%] sm:top-[-100%] sm:left-[100%] sm:-translate-x-1/2' />
         <div className='z-2'>{children}</div>
     </div>
 }
