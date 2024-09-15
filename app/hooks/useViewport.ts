3bdb73936615043e89674102af3f36ad8acfc9c7
Rob <robert@abbeybarn.com>
2024-09-15 18:57:33 +0100

    Contact Page for Mobile done

--- app/hooks/useViewport.ts
+++ app/hooks/useViewport.ts
@@ -15,3 +15,4 @@ export const useViewport = (): boolean | null => {
 
     return isMobile;
 };
+
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -6,6 +6,8 @@ import { ArrowButton } from "~/components/foundational/ArrowButton";
 import { json, LoaderFunctionArgs, redirect, type ActionFunctionArgs } from '@shopify/remix-oxygen';
 import { useLoaderData } from "@remix-run/react";
 
+import "~/styles/app.css"
+
 export async function action({ request, context }: ActionFunctionArgs) {
     const formData = await request.formData();
 
@@ -48,84 +50,166 @@ export default function Contact() {
     };
 
     return (
-        <div className="flex flex-row">
-            <div className="w-[80%]">
+        <>
+            <div className="mobile-component my-10">
+                <div className="mx-4">
+                    <h1 className="text-7xl text-jc-dark-blue text-center font-display">
+                        GET IN <br /> CONTACT
+                        <span className="text-jc-light-blue">&#46;</span></h1>
+                    <div className='w-16 mx-auto mb-4'><DashDivider className="h-[3px]" /></div>
+                    <div style={{ fontSize: '15px' }} className="text-jc-dark-blue text-center my-6">
+                        {data.paragraph}
+                    </div>
+                </div>
                 <iframe
                     width={"100%"}
-                    height={"100%"}
+                    height={"350px"}
                     style={{ border: 0 }}
                     loading="lazy"
                     src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJw70hYpU030cRICkT5XaCvjs&key=AIzaSyDq9Cf_nF6vcnU44kb80fyY3RUCZZw8BG8"
                 />
-            </div>
-            <div className="w-4/10 m-10 max-w-lg m-w-sm">
-                <h1 className="text-8xl text-jc-dark-blue font-display">
-                    GET IN <br /> CONTACT
-                    <span className="text-jc-light-blue">&#46;</span></h1>
-                <div className='w-16 -mt-2 mb-4'><DashDivider className="h-[3px]" /></div>
-                <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-3">
-                    <img src={MessageIcon} className="mr-2" />
-                    {data.number}
-                </div>
-                <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-4">
-                    <img src={EmailIcon} className="mr-2" />
-                    {data.email}
-                </div>
-                <div className="text-jc-dark-blue">
-                    {data.paragraph}
-                </div>
-                <DashDivider className="!w-full h-[1px] opacity-25" />
-                <form className="flex flex-col max-w-[unset]" aria-labelledby="contact-form" method="post" action="/contact">
-                    <h2 id="contact-form" className="sr-only">Contact Form</h2>
-                    <label className="mb-2 text-jc-dark-blue" htmlFor="fullName">FULL NAME</label>
-                    <input
-                        type="text"
-                        id="fullName"
-                        name="fullName"
-                        className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
-                        required
-                        aria-required="true"
-                        aria-describedby="fullNameHelp"
-                    />
+                <div className="my-4 mx-4">
+                    <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-3">
+                        <img src={MessageIcon} className="mr-2" />
+                        {data.number}
+                    </div>
+                    <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-4">
+                        <img src={EmailIcon} className="mr-2" />
+                        {data.email}
+                    </div>
 
-                    <label className="mb-2 text-jc-dark-blue" htmlFor="email">EMAIL*</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
-                        required
-                        aria-required="true"
-                        aria-describedby="emailHelp"
-                    />
-                    <label className="mb-2 text-jc-dark-blue" htmlFor="orderNumber">ORDER NUMBER / PRODUCT SKU</label>
-                    <input
-                        type="text"
-                        id="orderNumber"
-                        name="orderNumber"
-                        className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
-                        required
-                        aria-required="true"
-                        aria-describedby="orderNumberHelp"
-                    />
-                    <label className="mb-2 text-jc-dark-blue" htmlFor="message">YOUR MESSAGE*</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
-                        rows={4}
-                        required
-                        aria-required="true"
-                        aria-describedby="messageHelp"
-                    ></textarea>
-                    <ArrowButton className="w-max" type="submit" label={"Submit Message"} />
-                    {success && (
-                        <div className="mt-4 p-4 bg-green-100 text-green-800 border border-green-300 rounded">
-                            Your message has been successfully sent!
-                        </div>
-                    )}
-                </form>
+                    <DashDivider className="!w-full h-[1px] opacity-25" />
+                    <form className="flex flex-col max-w-[unset]" aria-labelledby="contact-form" method="post" action="/contact">
+                        <h2 id="contact-form" className="sr-only">Contact Form</h2>
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="fullName">FULL NAME</label>
+                        <input
+                            type="text"
+                            id="fullName"
+                            name="fullName"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="fullNameHelp"
+                        />
+
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="email">EMAIL*</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="emailHelp"
+                        />
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="orderNumber">ORDER NUMBER / PRODUCT SKU</label>
+                        <input
+                            type="text"
+                            id="orderNumber"
+                            name="orderNumber"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="orderNumberHelp"
+                        />
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="message">YOUR MESSAGE*</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            rows={4}
+                            required
+                            aria-required="true"
+                            aria-describedby="messageHelp"
+                        ></textarea>
+                        <ArrowButton className="w-max mx-auto" type="submit" label={"Submit Message"} />
+                        {success && (
+                            <div className="mt-4 p-4 bg-green-100 text-green-800 border border-green-300 rounded">
+                                Your message has been successfully sent!
+                            </div>
+                        )}
+                    </form>
+                </div>
             </div>
-        </div>
+            <div className="desktop-component flex flex-row">
+                <div className="w-[80%]">
+                    <iframe
+                        width={"100%"}
+                        height={"100%"}
+                        style={{ border: 0 }}
+                        loading="lazy"
+                        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJw70hYpU030cRICkT5XaCvjs&key=AIzaSyDq9Cf_nF6vcnU44kb80fyY3RUCZZw8BG8"
+                    />
+                </div>
+                <div className="w-4/10 m-10 max-w-lg m-w-sm">
+                    <h1 className="text-8xl text-jc-dark-blue font-display">
+                        GET IN <br /> CONTACT
+                        <span className="text-jc-light-blue">&#46;</span></h1>
+                    <div className='w-16 -mt-2 mb-4'><DashDivider className="h-[3px]" /></div>
+                    <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-3">
+                        <img src={MessageIcon} className="mr-2" />
+                        {data.number}
+                    </div>
+                    <div style={{ fontWeight: "700" }} className="flex items-center text-jc-dark-blue mb-4">
+                        <img src={EmailIcon} className="mr-2" />
+                        {data.email}
+                    </div>
+                    <div className="text-jc-dark-blue">
+                        {data.paragraph}
+                    </div>
+                    <DashDivider className="!w-full h-[1px] opacity-25" />
+                    <form className="flex flex-col max-w-[unset]" aria-labelledby="contact-form" method="post" action="/contact">
+                        <h2 id="contact-form" className="sr-only">Contact Form</h2>
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="fullName">FULL NAME</label>
+                        <input
+                            type="text"
+                            id="fullName"
+                            name="fullName"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="fullNameHelp"
+                        />
+
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="email">EMAIL*</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="emailHelp"
+                        />
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="orderNumber">ORDER NUMBER / PRODUCT SKU</label>
+                        <input
+                            type="text"
+                            id="orderNumber"
+                            name="orderNumber"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            required
+                            aria-required="true"
+                            aria-describedby="orderNumberHelp"
+                        />
+                        <label className="mb-2 text-jc-dark-blue" htmlFor="message">YOUR MESSAGE*</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="mb-4 p-2 bg-jc-light-grey-100 rounded border-none"
+                            rows={4}
+                            required
+                            aria-required="true"
+                            aria-describedby="messageHelp"
+                        ></textarea>
+                        <ArrowButton className="w-max" type="submit" label={"Submit Message"} />
+                        {success && (
+                            <div className="mt-4 p-4 bg-green-100 text-green-800 border border-green-300 rounded">
+                                Your message has been successfully sent!
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
+        </>
     );
 }
