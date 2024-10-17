9c326b8c44ce6f17faa1b8804b98a45a2b4542ec
Rob <robert@abbeybarn.com>
2024-10-17 23:39:01 +0100

    Needed to use loader in login

--- app/routes/account_.login.tsx
+++ app/routes/account_.login.tsx
@@ -1,10 +1,6 @@
-import { redirect, type ActionFunctionArgs } from '@shopify/remix-oxygen';
+import { LoaderFunctionArgs, redirect, type ActionFunctionArgs } from '@shopify/remix-oxygen';
 
 // if we dont implement this, /account/logout will get caught by account.$.tsx to do login
-export async function loader() {
-  return redirect('/');
-}
-
-export async function action({ request, context }: ActionFunctionArgs) {
+export async function loader({ context }: LoaderFunctionArgs) {
   return context.customerAccount.login();
 }
