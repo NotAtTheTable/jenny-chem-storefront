df4ca70ec5175b0aab9f33011246c38a14bd1307
Rob <robert@abbeybarn.com>
2024-10-20 14:55:47 +0100

    Added share social icons on the blog page

--- app/routes/blogs.$blogHandle.$articleHandle.tsx
+++ app/routes/blogs.$blogHandle.$articleHandle.tsx
@@ -2,10 +2,11 @@ import { json, type LoaderFunctionArgs } from '@shopify/remix-oxygen';
 import { Link, useLoaderData, type MetaFunction } from '@remix-run/react';
 import DashDivider from '~/components/foundational/DashDivider';
 import { PageHeader } from '~/components/foundational/PageHeader';
-import IgWhite from "~/assets/social-icons/instagram-white.svg";
+import LIWhite from "~/assets/social-icons/linkedin-white.svg";
 import FbWhite from "~/assets/social-icons/facebook-white.svg";
-import TTWhite from "~/assets/social-icons/tiktok-white.svg";
-import YTWhite from "~/assets/social-icons/youtube-white.svg";
+import XWhite from "~/assets/social-icons/x-white.svg";
+import PWhite from "~/assets/social-icons/pinterest-white.svg";
+import { Description } from '@radix-ui/react-dialog';
 
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
@@ -51,20 +52,20 @@ export default function Article() {
         subTextNode={
           <>
             <div style={{ fontSize: "18px" }} className='text'>{publishedDate} / {author?.name}</div>
-            {/* <div className='flex flex-row gap-2 items-center'>Share:
+            <div className='flex flex-row gap-2 items-center'>Share:
               <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`} target="_blank" rel="noopener noreferrer">
                 <img className='h-[18px]' src={FbWhite} alt="Share on Facebook" />
               </a>
-              <a href="https://www.instagram.com/officialjennychem" target="_blank" rel="noopener noreferrer">
-                <img className='h-[18px]' src={IgWhite} alt="Share on Instagram" />
+              <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(window.location.href)}`} target="_blank" rel="noopener noreferrer">
+                <img className='h-[18px]' src={XWhite} alt="Share on X" />
               </a>
-              <a href="https://www.tiktok.com/@jennychem.com" target="_blank" rel="noopener noreferrer">
-                <img className='h-[18px]' src={TTWhite} alt="Share on TikTok" />
+              <a href={`https://pinterest.com/pin/create/button/?url=${encodeURIComponent(window.location.href)}&description=${title}`} target="_blank" rel="noopener noreferrer">
+                <img className='h-[20px]' src={PWhite} alt="Share on Pinterest" />
               </a>
-              <a href="https://www.youtube.com/channel/UC5UXsZSRBnF7sVBi0egZz6g" target="_blank" rel="noopener noreferrer">
-                <img className='h-[18px]' src={YTWhite} alt="Share on YouTube" />
+              <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(window.location.href)}`} target="_blank" rel="noopener noreferrer">
+                <img className='h-[20px]' src={LIWhite} alt="Share on LinkedIn" />
               </a>
-            </div> */}
+            </div>
             <br />
             <div className='pb-6'><Link to={`/`}>Home</Link>&nbsp;&gt;&nbsp;<Link to={`/blogs/news`}>Blog</Link>&nbsp;&gt;&nbsp;{title}</div>
           </>
